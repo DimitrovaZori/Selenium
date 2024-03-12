064c0fb0a504834be7f74ebd566f79d687d49e17
Dimitrova.Zori <ub01632@gmail.com>
2024-03-12 22:48:41 +0200

    Additional

--- src/test/java/TestSite.java
+++ src/test/java/TestSite.java
@@ -16,5 +16,30 @@ public class TestSite {
         username.sendKeys("standard_user");
         password.sendKeys("secret_sauce");
         login.click();
+
+        WebElement tshirt = driver.findElement(By.xpath("//*[contains(text(), 'Sauce Labs Backpack')]"));
+        tshirt.click();
+        WebElement button = driver.findElement(By.cssSelector("button[class ='btn_primary btn_inventory']"));
+        button.click();
+        WebElement path = driver.findElement(By.cssSelector("path[fill='currentColor']"));
+        path.click();
+
+        WebElement checkout = driver.findElement(By.cssSelector("a[class*='checkout']"));
+        checkout.click();
+
+        //Page details
+
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        WebElement zipCode = driver.findElement(By.id("postal-code"));
+        firstName.sendKeys("Zori");
+        lastName.sendKeys("Dimitrova");
+        zipCode.sendKeys("9000");
+
+        WebElement submit = driver.findElement(By.cssSelector("[type='submit']"));
+        submit.click();
+
+        WebElement finish = driver.findElement(By.cssSelector("a[href*='checkout']"));
+        finish.click();
     }
 }
